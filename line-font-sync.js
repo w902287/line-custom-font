38569c93221f1859e4d22d1522db6f2a0ad4f0b7
w902287 <w902287@users.noreply.github.com>
2026-08-16 17:58:12 +0800

    fix(script): update FONT_URL to commit 0691cc7

--- line-font-sync.js
+++ line-font-sync.js
@@ -2,7 +2,7 @@
  * LINE Custom Font Local Injector for Quantumult X
  * 直接替換 Response Body
  */
-const FONT_URL = "https://raw.githubusercontent.com/w902287/line-custom-font/2bde2f430dc3faa1f7b7d9fa13e137411588b95d/TT07-armochih32c90b5_md_scale.zip";
+const FONT_URL = "https://raw.githubusercontent.com/w902287/line-custom-font/0691cc7bb1939eb8a56a9b3665562b5f68cfb2af/TT07-armochih32c90b5_md_scale.zip";
 
 console.log("[LINE-Font] 攔截到字型下載請求：" + $request.url);
 
